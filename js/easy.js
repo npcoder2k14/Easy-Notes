e77c103fabe11536ab6fb1002eb04fb974d97df4
Mayank Pratap <mayank7860@gmail.com>
2016-07-04 18:58:21 +0530

    A basic template showing required toobar

---
+++ js/easy.js
@@ -0,0 +1,6 @@
+tinymce.init({
+
+  selector: 'textarea',  // note the comma at the end of the line!
+  toolbar: 'newdocument | undo redo | formatselect fontselect fontsizeselect | bold italic | alignleft aligncenter alignright',
+  menubar: false
+});
